26ce23d980f9a08510726d165c2d41b3afc76c2d
Plangloi <48372629+Plangloi@users.noreply.github.com>
2024-05-08 13:22:10 -0400

    Update Proxy_CR.sh
    New Version

--- Proxy_CR.sh
+++ Proxy_CR.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
-
+#v1_ok
 # Add proxy configuration to /etc/apt/apt.conf
+#sudo bash -c 'echo "Acquire::http::proxy \"http://10.1.0.5:8080/\";" >> /etc/apt/apt.conf'
+#sudo bash -c 'echo "Acquire::https::proxy \"https://10.1.0.5:8080/\";" >> /etc/apt/apt.conf'
+
+# Add proxy configuration to ~/.bashrc
+#echo 'export http_proxy=http://10.1.0.5:8080' >> ~/.bashrc
+#echo 'export https_proxy=http://10.1.0.5:8080' >> ~/.bashrc
+#echo 'export ftp_proxy=http://10.1.0.5:8080' >> ~/.bashrc
+
+# Source the ~/.bashrc file to apply changes immediately
+#source ~/.bashrc
+
+#echo "Proxy settings have been applied successfully."
+=========================================================================================
+#New_v2
+# Add proxy configuration to /etc/apt/apt.conf
+if [ ! -f "/etc/apt/apt.conf" ]; then
+    sudo touch /etc/apt/apt.conf
+fi
 sudo bash -c 'echo "Acquire::http::proxy \"http://10.1.0.5:8080/\";" >> /etc/apt/apt.conf'
 sudo bash -c 'echo "Acquire::https::proxy \"https://10.1.0.5:8080/\";" >> /etc/apt/apt.conf'
+sudo bash -c 'echo "Acquire::ftp::proxy \"ftp://10.1.0.5:8080/\";" >> /etc/apt/apt.conf'
 
 # Add proxy configuration to ~/.bashrc
 echo 'export http_proxy=http://10.1.0.5:8080' >> ~/.bashrc
 echo 'export https_proxy=http://10.1.0.5:8080' >> ~/.bashrc
-echo 'export ftp_proxy=http://10.1.0.5:8080' >> ~/.bashrc
+echo 'export ftp_proxy=ftp://10.1.0.5:8080' >> ~/.bashrc
 
 # Source the ~/.bashrc file to apply changes immediately
 source ~/.bashrc
 
-echo "Proxy settings have been applied successfully."
+# Option to remove proxy configuration
+if [ "$1" = "--remove" ]; then
+    sudo bash -c 'sed -i "/Acquire::http/d" /etc/apt/apt.conf'
+    sudo bash -c 'sed -i "/Acquire::https/d" /etc/apt/apt.conf'
+    sudo bash -c 'sed -i "/Acquire::ftp/d" /etc/apt/apt.conf'
+    echo "Proxy settings have been removed successfully."
+fi
